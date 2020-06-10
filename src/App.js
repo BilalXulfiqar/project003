f9d6917d393a637e73a20d583fb58ae5e82b0c1a
BilalXulfiqar <60869198+BilalXulfiqar@users.noreply.github.com>
2020-06-10 20:44:14 +0500

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Dinner from './dinner'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    < Dinner />
   );
 }
 
---
+++ src/dinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dinner(){
+
+    
+   <h1>We have Biryani</h1>
+
+
+
+}
+
+export default Dinner;
